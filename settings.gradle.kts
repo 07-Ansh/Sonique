d711edc3607aab911cb737c66b534ad508428147
Ansh Sharma <official.ansh@icloud.com>
2026-01-02 22:09:55 +0530

    removed unused modules
    Removed the unused modules to clean up the project structure.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -83,8 +83,7 @@ include(
     ":sharedutils",
     ":media3",
     ":media3-ui",
-    ":crashlytics",
-    ":crashlytics-empty",
+
 )
 project(":mediaserviceinterfaces").projectDir = File(mediaServiceCore, "mediaserviceinterfaces")
 project(":youtubeapi").projectDir = File(mediaServiceCore, "youtubeapi")

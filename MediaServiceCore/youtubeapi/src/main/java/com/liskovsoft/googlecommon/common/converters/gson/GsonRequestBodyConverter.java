c8083b5133095246c927c77895ec18dc2f6a260f
Ansh Sharma <official.ansh@icloud.com>
2026-01-08 14:00:00 +0530

    style: polish code formatting and imports in GsonRequestBodyConverter.java

--- MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/googlecommon/common/converters/gson/GsonRequestBodyConverter.java
+++ MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/googlecommon/common/converters/gson/GsonRequestBodyConverter.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2015 Square, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ 
 package com.liskovsoft.googlecommon.common.converters.gson;
 
 import com.google.gson.Gson;
@@ -38,18 +24,18 @@ final class GsonRequestBodyConverter<T> implements Converter<T, RequestBody> {
   }
 
   @Override public RequestBody convert(T value) throws IOException {
-    // Original code
-    //Buffer buffer = new Buffer();
-    //Writer writer = new OutputStreamWriter(buffer.outputStream(), UTF_8);
-    //JsonWriter jsonWriter = gson.newJsonWriter(writer);
-    //adapter.write(jsonWriter, value);
-    //jsonWriter.close();
-    //return RequestBody.create(MEDIA_TYPE, buffer.readByteString());
+     
+     
+     
+     
+     
+     
+     
 
-    // Accept raw json body
-    //return RequestBody.create(MEDIA_TYPE, value.toString());
+     
+     
 
-    // Accept raw json body as a String
+     
     return RequestBody.create(MEDIA_TYPE, value instanceof String ? (String) value : gson.toJson(value));
   }
 }

40d91078d8967370a71fa97727439972fb1ee47e
Ansh Sharma <official.ansh@icloud.com>
2026-01-08 13:09:00 +0530

    style: polish code formatting and imports in TrackingApiTest.java

--- MediaServiceCore/youtubeapi/src/androidTest/java/com/liskovsoft/youtubeapi/track/TrackingApiTest.java
+++ MediaServiceCore/youtubeapi/src/androidTest/java/com/liskovsoft/youtubeapi/track/TrackingApiTest.java
@@ -33,7 +33,7 @@ public class TrackingApiTest {
 
     @Before
     public void setUp() throws InterruptedException {
-        // Fix temp video url ban
+         
         Thread.sleep(3_000);
 
         GlobalPreferences.instance(InstrumentationRegistry.getInstrumentation().getContext());
@@ -47,17 +47,17 @@ public class TrackingApiTest {
         RetrofitOkHttpHelper.getAuthHeaders().put("Authorization", TestHelpers.getAuthorization());
     }
 
-    //@Test
-    //public void testCreateWatchRecord() throws IOException {
-    //    Response<WatchTimeEmptyResult> response = createWatchRecord(TestHelpers.VIDEO_ID_SIMPLE);
-    //
-    //    assertTrue("Create watch record response successful", response.isSuccessful());
-    //
-    //    BrowseResult history = mBrowseServiceSigned.getHistory(sAuthorization);
-    //    String newVideoId = history.getVideoItems().get(0).getVideoId();
-    //
-    //    assertEquals("History updated", TestHelpers.VIDEO_ID_SIMPLE, newVideoId);
-    //}
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
 
     @Test
     public void testUpdateWatchTime() throws IOException {
@@ -87,9 +87,9 @@ public class TrackingApiTest {
 
         assertEquals("History updated", videoIdSimple, historyItem.getVideoId());
 
-        //WatchNextResult watchNextResult = mWatchNextServiceSigned.getWatchNextResult(videoIdSimple, sAuthorization);
-        //
-        //int percentWatched = watchNextResult.getVideoMetadata().getPercentWatched();
+         
+         
+         
 
         int percentWatched = historyItem.getPercentWatched();
 
@@ -97,7 +97,7 @@ public class TrackingApiTest {
     }
 
     private Response<WatchTimeEmptyResult> createWatchRecord(String videoId, String playbackNonce) throws IOException {
-        //String playbackNonce = mAppService.getClientPlaybackNonce();
+         
         VideoInfo videoInfo = mVideoInfoService.getVideoInfo(videoId, null);
 
         Call<WatchTimeEmptyResult> wrapper = mTrackingApi.createWatchRecordShort(
@@ -106,20 +106,20 @@ public class TrackingApiTest {
         return wrapper.execute();
     }
 
-    //private Response<WatchTimeEmptyResult> createWatchRecord(String videoId, float positionSec) throws IOException {
-    //    String playbackNonce = mAppService.getClientPlaybackNonce();
-    //    VideoInfoResult videoInfo = mVideoInfoServiceSigned.getVideoInfo(videoId, sAuthorization);
-    //
-    //    Call<WatchTimeEmptyResult> wrapper = mTrackingManager.createWatchRecord(
-    //            videoId, Float.parseFloat(videoInfo.getVideoDetails().getLengthSeconds()), positionSec, positionSec, positionSec,
-    //            playbackNonce, videoInfo.getEventId(), videoInfo.getVisitorMonitoringData(), sAuthorization
-    //    );
-    //
-    //    return wrapper.execute();
-    //}
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
 
     private Response<WatchTimeEmptyResult> createWatchRecord(String videoId, float positionSec, String playbackNonce) throws IOException {
-        //String playbackNonce = mAppService.getClientPlaybackNonce();
+         
         VideoInfo videoInfo = mVideoInfoService.getAuthVideoInfo(videoId, null);
 
         Call<WatchTimeEmptyResult> wrapper = mTrackingApi.createWatchRecord(
@@ -130,7 +130,7 @@ public class TrackingApiTest {
     }
 
     private Response<WatchTimeEmptyResult> updateWatchTime(String videoId, float positionSec, String playbackNonce) throws IOException {
-        //String playbackNonce = mAppService.getClientPlaybackNonce();
+         
         VideoInfo videoInfo = mVideoInfoService.getAuthVideoInfo(videoId, null);
 
         Call<WatchTimeEmptyResult> wrapper = mTrackingApi.updateWatchTime(
@@ -141,7 +141,7 @@ public class TrackingApiTest {
     }
 
     private Response<WatchTimeEmptyResult> updateWatchTimeAlt(String videoId, float positionSec, String playbackNonce) throws IOException {
-        //String playbackNonce = mAppService.getClientPlaybackNonce();
+         
         VideoInfo videoInfo = mVideoInfoService.getVideoInfo(videoId, null);
 
         Call<WatchTimeEmptyResult> wrapper = mTrackingApi.updateWatchTimeShort(

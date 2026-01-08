dbd4360bdb53622bbc105e506c19da65a04dcff7
Ansh Sharma <official.ansh@icloud.com>
2026-01-08 09:45:00 +0530

    style: polish code formatting and imports in DownloadManagerTask.java

--- MediaServiceCore/SharedModules/appupdatechecker2/src/main/java/com/liskovsoft/appupdatechecker2/other/downloadmanager/DownloadManagerTask.java
+++ MediaServiceCore/SharedModules/appupdatechecker2/src/main/java/com/liskovsoft/appupdatechecker2/other/downloadmanager/DownloadManagerTask.java
@@ -13,40 +13,21 @@ public class DownloadManagerTask extends AsyncTask<Void, Integer, Integer> {
     private static final String TAG = DownloadManagerTask.class.getSimpleName();
     private static final String DEFAULT_DOWNLOAD_FILE_NAME = "tmp.apk";
 
-    /**
-     * Interface used to download the Crosswalk runtime
-     */
+     
     public static abstract class DownloadListener {
-        /**
-         * Run on the UI thread to notify download is started
-         */
+         
         public void onDownloadStarted() {}
 
-        /**
-         * Run on the UI thread to notify the download progress
-         * @param percentage Shows the download progress in percentage
-         */
+         
         public void onDownloadUpdated(int percentage) {}
 
-        /**
-         * Run on the UI thread to notify download is cancelled
-         */
+         
         public void onDownloadCancelled() {}
 
-        /**
-         * Run on the UI thread to notify download is completed successfully
-         * @param uri The Uri where the downloaded file is stored
-         */
+         
         public void onDownloadCompleted(Uri uri) {}
 
-        /**
-         * Run on the UI thread to notify download failed
-         *
-         * @param status The download status defined in android.app.DownloadManager.
-         *               The value maybe STATUS_FAILED or STATUS_PAUSED
-         * @param error The download error defined in android.app.DownloadManager.
-         *              This parameter only makes sense when the status is STATUS_FAILED
-         */
+         
         public void onDownloadFailed(int status, int error) {}
     }
 
@@ -95,7 +76,7 @@ public class DownloadManagerTask extends AsyncTask<Void, Integer, Integer> {
             mDownloadId = mDownloadManager.enqueue(request);
         } catch (IllegalStateException ex) {
             Log.e(TAG, ex.getMessage(), ex);
-            //MessageHelpers.showMessage(mContext, TAG, ex);
+             
         }
 
         return isDone ? android.app.DownloadManager.STATUS_SUCCESSFUL : android.app.DownloadManager.STATUS_FAILED;
@@ -134,7 +115,7 @@ public class DownloadManagerTask extends AsyncTask<Void, Integer, Integer> {
                 }
             } catch (IllegalStateException ex) {
                 Log.e(TAG, ex.getMessage(), ex);
-                //MessageHelpers.showMessage(mContext, TAG, ex);
+                 
                 mListener.onDownloadFailed(result, android.app.DownloadManager.ERROR_UNKNOWN);
             }
         } else {

bbf3c53534773ee8b9c4f9629c83776c4efee280
Ansh Sharma <official.ansh@icloud.com>
2026-01-08 12:03:00 +0530

    style: polish code formatting and imports in UrlEncodedQueryStringTest.java

--- MediaServiceCore/SharedModules/sharedutils/src/test/java/com/liskovsoft/sharedutils/querystringparser/UrlEncodedQueryStringTest.java
+++ MediaServiceCore/SharedModules/sharedutils/src/test/java/com/liskovsoft/sharedutils/querystringparser/UrlEncodedQueryStringTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertNotNull;
 
 @RunWith(RobolectricTestRunner.class)
 public class UrlEncodedQueryStringTest {
-    // Russian is not working. Query is changed after parse.
+     
     private static final String TEST_URL_1 = "https://www.youtube.com/results?search_query=Джентльмены удачи";
     private static final String TEST_URL_2 = "https://www.youtube.com/results?search_query=Hello+World";
     private static final String TEST_URL_3 = "search_query=Hello+World";
@@ -17,10 +17,10 @@ public class UrlEncodedQueryStringTest {
 
     @Test
     public void testThatQueryParsedCorrectly() {
-        //testUrl(TEST_URL_1);
+         
         testUrl(TEST_URL_2);
         testUrl(TEST_URL_3);
-        //testUrl(TEST_URL_4);
+         
     }
 
     private void testUrl(String url) {

4c0370716d0eac9c3e50756b03b5f8cd4fe136d8
Ansh Sharma <official.ansh@icloud.com>
2026-01-08 15:00:00 +0530

    style: polish code formatting and imports in UserCode.java

--- MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/googlecommon/common/models/auth/UserCode.java
+++ MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/googlecommon/common/models/auth/UserCode.java
@@ -2,29 +2,20 @@ package com.liskovsoft.googlecommon.common.models.auth;
 
 import com.liskovsoft.googlecommon.common.converters.jsonpath.JsonPath;
 
-/**
- * Manual: https://developers.google.com/identity/protocols/oauth2/limited-input-device#success-response
- */
+ 
 public class UserCode {
-    /**
-     * Example: AH-1Ng1K8lBZcrMezwnEzpZ23VwfE8Hn7HEuNL-GbQIMYrSZhBF1j3KZ-h47-bqBB4rC6-W9xG2WHM67ZGttecz_ALOZ-JoxDQ
-     */
+     
     @JsonPath("$.device_code")
     private String mDeviceCode;
 
-    /**
-     * Code to enter into the browser page at mVerificationUrl<br/>
-     * Example: XWY-QRL-MNH
-     */
+     
     @JsonPath("$.user_code")
     private String mUserCode;
 
     @JsonPath("$.expires_in")
     private int mExpiresIn;
 
-    /**
-     * In seconds to poll the server
-     */
+     
     @JsonPath("$.interval")
     private int mInterval;
     
@@ -42,7 +33,7 @@ public class UserCode {
             return null;
         }
 
-        // Make code more readable by removing unused characters
+         
         if (mUserCodePretty == null) {
             mUserCodePretty = mUserCode.replace("-", " ");
         }

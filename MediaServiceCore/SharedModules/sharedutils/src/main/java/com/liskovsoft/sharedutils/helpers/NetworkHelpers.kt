1de0f68096260adeb741cd305109903e09275c30
Ansh Sharma <official.ansh@icloud.com>
2026-01-08 10:39:00 +0530

    style: polish code formatting and imports in NetworkHelpers.kt

--- MediaServiceCore/SharedModules/sharedutils/src/main/java/com/liskovsoft/sharedutils/helpers/NetworkHelpers.kt
+++ MediaServiceCore/SharedModules/sharedutils/src/main/java/com/liskovsoft/sharedutils/helpers/NetworkHelpers.kt
@@ -12,22 +12,18 @@ import javax.net.ssl.HttpsURLConnection
 object NetworkHelpers {
     @JvmStatic
     fun getHttpsURLConnection(url: URL): HttpURLConnection {
-        // Original value
-        //val conn = url.openConnection() as HttpURLConnection
+         
+         
         val conn = getBestHttpsURLConnection(url)
 
-        // Imitate 'keepAlive' = false (cause buffering?)
-        // https://stackoverflow.com/questions/3352424/httpurlconnection-openconnection-fails-second-time/3943820#3943820
-        //conn.setRequestProperty("connection", "close")
+         
+         
+         
 
         return conn
     }
 
-    /**
-     * Doesn't work with ip (HTTP/1.1 400 Bad Request)
-     *
-     * Server rejects connection by ip
-     */
+     
     @JvmStatic
     fun getDohURLConnection(url: URL): HttpURLConnection {
         if (Build.VERSION.SDK_INT <= 19) {
@@ -41,7 +37,7 @@ object NetworkHelpers {
             Log.d("NetworkHelpers", fullUrl)
 
             val conn = getBestHttpsURLConnection(URL(fullUrl))
-            // fix SSLPeerUnverifiedException (because we're connecting by ip)
+             
             conn.hostnameVerifier = HostnameVerifier { _, _ -> true }
             conn
         } ?: getBestHttpsURLConnection(url)

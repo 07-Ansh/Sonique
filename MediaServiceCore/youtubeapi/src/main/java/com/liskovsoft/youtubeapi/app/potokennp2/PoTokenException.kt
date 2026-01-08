b1f810eb763bb993222bbf7a0dd74aa87eab9f0c
Ansh Sharma <official.ansh@icloud.com>
2026-01-08 16:30:00 +0530

    style: polish code formatting and imports in PoTokenException.kt

--- MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/potokennp2/PoTokenException.kt
+++ MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/potokennp2/PoTokenException.kt
@@ -2,7 +2,7 @@ package com.liskovsoft.youtubeapi.app.potokennp2
 
 internal class PoTokenException(message: String) : RuntimeException(message)
 
-// to be thrown if the WebView provided by the system is broken
+ 
 internal class BadWebViewException(message: String) : RuntimeException(message)
 
 internal fun buildExceptionForJsError(error: String): Throwable {

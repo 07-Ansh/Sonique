7e271e8addcd92a708a3ec0c8302c917754500f6
Ansh Sharma <official.ansh@icloud.com>
2026-01-09 14:48:00 +0530

    style: polish code formatting and imports in OAuth2ApiTest.java

--- MediaServiceCore/youtubeapi/src/test/java/com/liskovsoft/googleapi/oauth2/OAuth2ApiTest.java
+++ MediaServiceCore/youtubeapi/src/test/java/com/liskovsoft/googleapi/oauth2/OAuth2ApiTest.java
@@ -27,11 +27,11 @@ public class OAuth2ApiTest {
 
     @Before
     public void setUp() {
-        // fix issue: No password supplied for PKCS#12 KeyStore
-        // https://github.com/robolectric/robolectric/issues/5115
+         
+         
         System.setProperty("javax.net.ssl.trustStoreType", "JKS");
 
-        ShadowLog.stream = System.out; // catch Log class output
+        ShadowLog.stream = System.out;  
 
         mService = RetrofitHelper.create(OAuth2Api.class);
 
@@ -69,18 +69,18 @@ public class OAuth2ApiTest {
         }
     }
 
-    //@Test
-    //public void testThatAccountsListNotEmpty() {
-    //    Call<AccountsList> wrapper = mService.getAccountsList(OAuth2ApiHelper.getAccountsListQuery());
-    //
-    //    AccountsList accountsList = RetrofitHelper.get(wrapper);
-    //
-    //    AccountInt firstAccount = accountsList.getAccounts().get(0);
-    //
-    //    assertNotNull("Contains Name", firstAccount.getName());
-    //    assertNotNull("Contains Thumbnails", firstAccount.getThumbnails());
-    //    assertTrue("Is selected", firstAccount.isSelected());
-    //}
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
 
     private boolean notEmpty(String userCode) {
         System.out.println("Important code is: " + userCode);

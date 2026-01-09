12d7614cd26375abe2be984acb33506ef35115bc
Ansh Sharma <official.ansh@icloud.com>
2026-01-09 13:06:00 +0530

    style: polish code formatting and imports in SearchResult.java

--- MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/youtubeapi/search/models/SearchResult.java
+++ MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/youtubeapi/search/models/SearchResult.java
@@ -6,26 +6,22 @@ import com.liskovsoft.youtubeapi.common.models.items.ItemWrapper;
 import java.util.List;
 
 public class SearchResult {
-    //@JsonPath("$.contents.sectionListRenderer.contents[0].itemSectionRenderer.contents[*]")
-    //private List<ItemWrapper> mItemWrappers;
-    //
-    ///**
-    // * Nowadays, search result may contains two rows.<br/>
-    // * First of them with continuation is interested for us.
-    // */
-    //@JsonPath({"$.contents.sectionListRenderer.contents[0].itemSectionRenderer.continuations[0].nextContinuationData.continuation",
-    //           "$.contents.sectionListRenderer.contents[1].itemSectionRenderer.continuations[0].nextContinuationData.continuation"})
-    //private String mNextPageKey;
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
 
-    /**
-     * Search result with multiple rows
-     */
+     
     @JsonPath("$.contents.sectionListRenderer.contents[*]")
     private List<SearchSection> mSections;
 
-    /**
-     * Presents even when there is no results
-     */
+     
     @JsonPath("$.estimatedResults")
     private String mEstimatedResults;
 

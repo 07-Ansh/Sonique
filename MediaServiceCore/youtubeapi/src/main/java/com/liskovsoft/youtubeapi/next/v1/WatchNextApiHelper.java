3f1ab756150dbced4df26b0975bc024082830bde
Ansh Sharma <official.ansh@icloud.com>
2026-01-09 12:03:00 +0530

    style: polish code formatting and imports in WatchNextApiHelper.java

--- MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/youtubeapi/next/v1/WatchNextApiHelper.java
+++ MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/youtubeapi/next/v1/WatchNextApiHelper.java
@@ -13,17 +13,13 @@ public class WatchNextApiHelper {
         return getWatchNextQuery(videoId, playlistId, playlistIndex, null);
     }
 
-    /**
-     * Video query from playlist example: "params":"OAI%3D","playlistId":"RDx7g_SWE90O8","playlistIndex": 2,"videoId":"x7g_SWE90O8"<br/>
-     * Video query example: "videoId":"x7g_SWE90O8"<br/>
-     * PlaylistParams is used inside browse channel queries
-     */
+     
     public static String getWatchNextQuery(@Nullable String videoId, @Nullable String playlistId, int playlistIndex, String playlistParams) {
-        // always presents
+         
         String videoData = String.format("\"videoId\":\"%s\"", videoId);
 
-        // present only on play lists
-        // sometimes "params" present too: "params":"OAI%3D"
+         
+         
         if (playlistId != null) {
             videoData += String.format(",\"playlistId\":\"%s\",\"playlistIndex\":%s", playlistId, Math.max(playlistIndex, 0));
         }

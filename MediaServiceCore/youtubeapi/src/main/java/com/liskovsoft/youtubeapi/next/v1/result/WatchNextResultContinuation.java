3322fb8bc93bb65da2a5399deab13d013808ffb2
Ansh Sharma <official.ansh@icloud.com>
2026-01-09 12:18:00 +0530

    style: polish code formatting and imports in WatchNextResultContinuation.java

--- MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/youtubeapi/next/v1/result/WatchNextResultContinuation.java
+++ MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/youtubeapi/next/v1/result/WatchNextResultContinuation.java
@@ -12,10 +12,7 @@ public class WatchNextResultContinuation {
                "$.continuationContents.horizontalListContinuation.continuations[1].nextContinuationData.continuation"})
     private String mNextPageKey;
 
-    /**
-     * Marker field to avoid json parser empty result warning.<br/>
-     * It appeared when we reached end of the list.<br/>
-     */
+     
     @JsonPath("$.contents.singleColumnWatchNextResults.results.results.trackingParams")
     private String mTrackingParams;
 

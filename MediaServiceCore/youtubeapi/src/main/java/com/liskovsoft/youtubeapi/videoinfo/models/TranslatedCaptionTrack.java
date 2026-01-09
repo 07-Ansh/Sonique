5dcd9f2c8b540bf0ffb0cbe7104f5251ef9ba730
Ansh Sharma <official.ansh@icloud.com>
2026-01-09 14:30:00 +0530

    style: polish code formatting and imports in TranslatedCaptionTrack.java

--- MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/youtubeapi/videoinfo/models/TranslatedCaptionTrack.java
+++ MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/youtubeapi/videoinfo/models/TranslatedCaptionTrack.java
@@ -17,7 +17,7 @@ public class TranslatedCaptionTrack extends CaptionTrack {
 
     @Override
     public String getBaseUrl() {
-        // Don't try to translate the same lang or you'll get a mess
+         
         if (Helpers.equals(mOriginTrack.getLanguageCode(), mLanguage.getLanguageCode())) {
             return mOriginTrack.getBaseUrl();
         }
@@ -42,8 +42,8 @@ public class TranslatedCaptionTrack extends CaptionTrack {
 
     @Override
     public String getName() {
-        // NOTE: tag contain weird chars: (simplified) - chinese (simplified)
-        //return mLanguage.getLanguageName() + (mTag != null ? " " + mTag : "") + TRANSLATE_MARKER;
+         
+         
 
         return YouTubeHelper.exoNameFix(mLanguage.getLanguageName()) + TRANSLATE_MARKER;
     }
@@ -63,7 +63,7 @@ public class TranslatedCaptionTrack extends CaptionTrack {
         return mOriginTrack.getCodecs();
     }
 
-    // Doesn't work!!!!
+     
     private String countryCodeToFlag(String countryCode) {
         int firstLetter = Character.codePointAt(countryCode, 0) - 0x41 + 0x1F1E6;
         int secondLetter = Character.codePointAt(countryCode, 1) - 0x41 + 0x1F1E6;

469fe5076facefb9c09ac80b5e3926b622a6488f
Ansh Sharma <official.ansh@icloud.com>
2026-01-09 12:39:00 +0530

    style: polish code formatting and imports in PlaylistApi.java

--- MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/youtubeapi/playlist/PlaylistApi.java
+++ MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/youtubeapi/playlist/PlaylistApi.java
@@ -8,9 +8,7 @@ import retrofit2.http.Body;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
-/**
- * For signed users!
- */
+ 
 @WithJsonPath
 public interface PlaylistApi {
     @Headers("Content-Type: application/json")
@@ -21,16 +19,12 @@ public interface PlaylistApi {
     @POST("https://www.youtube.com/youtubei/v1/browse/edit_playlist")
     Call<ActionResult> editPlaylist(@Body String editPlaylistQuery);
 
-    /**
-     * Works with foreign playlists
-     */
+     
     @Headers("Content-Type: application/json")
     @POST("https://www.youtube.com/youtubei/v1/like/like")
     Call<ActionResult> saveForeignPlaylist(@Body String saveRemoveForeignPlaylistQuery);
 
-    /**
-     * Works with foreign playlists
-     */
+     
     @Headers("Content-Type: application/json")
     @POST("https://www.youtube.com/youtubei/v1/like/removelike")
     Call<ActionResult> removeForeignPlaylist(@Body String saveRemoveForeignPlaylistQuery);
@@ -39,9 +33,7 @@ public interface PlaylistApi {
     @POST("https://www.youtube.com/youtubei/v1/playlist/create")
     Call<ActionResult> createPlaylist(@Body String createPlaylistQuery);
 
-    /**
-     * Works with user playlists
-     */
+     
     @Headers("Content-Type: application/json")
     @POST("https://www.youtube.com/youtubei/v1/playlist/delete")
     Call<ActionResult> removePlaylist(@Body String removePlaylistQuery);

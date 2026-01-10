403b9213d1ad64b9026f7c57f2b6abe4c19eeff4
Ansh Sharma <official.ansh@icloud.com>
2026-01-10 15:48:00 +0530

    style: polish code formatting and imports in Utils.kt

--- core/service/kotlinYtmusicScraper/src/commonMain/kotlin/com/sonique/kotlinytmusicscraper/utils/Utils.kt
+++ core/service/kotlinYtmusicScraper/src/commonMain/kotlin/com/sonique/kotlinytmusicscraper/utils/Utils.kt
@@ -47,11 +47,11 @@ fun generateNetscapeCookies(
     val cookieLines =
         cookies
             .map { (name, value) ->
-                // Netscape format: domain, domainFlag, path, secure, expiration, name, value
+                 
                 buildString {
                     append(domain)
                     append("\t")
-                    append("TRUE") // domain flag - TRUE means domain includes subdomains
+                    append("TRUE")  
                     append("\t")
                     append(path)
                     append("\t")

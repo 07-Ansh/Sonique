9b3a6a860c9e49e6e0ad9c027f81749b1d15243d
Ansh Sharma <official.ansh@icloud.com>
2026-07-19 17:24:41 +0530

    Route all list screen graph destinations to DetailContentScreen

--- composeApp/src/commonMain/kotlin/com/sonique/app/ui/navigation/graph/ListScreenGraph.kt
+++ composeApp/src/commonMain/kotlin/com/sonique/app/ui/navigation/graph/ListScreenGraph.kt
@@ -20,6 +20,9 @@ import com.sonique.app.ui.screen.other.MoreAlbumsScreen
 import com.sonique.app.ui.screen.other.PlaylistScreen
 import com.sonique.app.ui.screen.other.PodcastScreen
 
+import com.sonique.app.ui.screen.other.DetailContentScreen
+import com.sonique.app.ui.screen.other.DetailType
+
 @ExperimentalMaterial3Api
 @ExperimentalFoundationApi
 fun NavGraphBuilder.listScreenGraph(
@@ -29,52 +32,64 @@ fun NavGraphBuilder.listScreenGraph(
 ) {
     composable<AlbumDestination> { entry ->
         val data = entry.toRoute<AlbumDestination>()
-        AlbumScreen(
-            browseId = data.browseId,
+        DetailContentScreen(
+            type = DetailType.ALBUM,
+            id = data.browseId,
             navController = navController,
             onScrolling = onScrolling,
+            innerPadding = innerPadding
         )
     }
     composable<ArtistDestination> { entry ->
         val data = entry.toRoute<ArtistDestination>()
-        ArtistScreen(
-            channelId = data.channelId,
+        DetailContentScreen(
+            type = DetailType.ARTIST,
+            id = data.channelId,
             navController = navController,
             onScrolling = onScrolling,
+            innerPadding = innerPadding
         )
     }
     composable<LocalPlaylistDestination> { entry ->
         val data = entry.toRoute<LocalPlaylistDestination>()
-        LocalPlaylistScreen(
+        DetailContentScreen(
+            type = DetailType.LOCAL_PLAYLIST,
             id = data.id,
             navController = navController,
             onScrolling = onScrolling,
+            innerPadding = innerPadding
         )
     }
     composable<MoreAlbumsDestination> { entry ->
         val data = entry.toRoute<MoreAlbumsDestination>()
-        MoreAlbumsScreen(
-            innerPadding = innerPadding,
+        DetailContentScreen(
+            type = DetailType.MORE_ALBUMS,
+            id = data.type,
+            secondaryId = data.id,
             navController = navController,
-            type = data.type,
-            id = data.id,
+            onScrolling = onScrolling,
+            innerPadding = innerPadding
         )
     }
     composable<PlaylistDestination> { entry ->
         val data = entry.toRoute<PlaylistDestination>()
-        PlaylistScreen(
-            playlistId = data.playlistId,
+        DetailContentScreen(
+            type = DetailType.YOUTUBE_PLAYLIST,
+            id = data.playlistId,
             isYourYouTubePlaylist = data.isYourYouTubePlaylist,
             navController = navController,
             onScrolling = onScrolling,
+            innerPadding = innerPadding
         )
     }
     composable<PodcastDestination> { entry ->
         val data = entry.toRoute<PodcastDestination>()
-        PodcastScreen(
-            podcastId = data.podcastId,
+        DetailContentScreen(
+            type = DetailType.PODCAST,
+            id = data.podcastId,
             navController = navController,
             onScrolling = onScrolling,
+            innerPadding = innerPadding
         )
     }
 }

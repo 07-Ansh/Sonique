fca42061e02d490fbf608c0783a0545885ba833f
Ansh Sharma <official.ansh@icloud.com>
2026-09-06 20:20:38 +0530

    Add Capturable composable for offscreen image rendering

--- composeApp/src/androidMain/kotlin/com/sonique/app/expect/ui/Bitmap.android.kt
+++ composeApp/src/androidMain/kotlin/com/sonique/app/expect/ui/Bitmap.android.kt
@@ -1,6 +1,7 @@
 package com.sonique.app.expect.ui
 
 import android.graphics.Bitmap.CompressFormat.JPEG
+import android.graphics.Bitmap.CompressFormat.PNG
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.graphics.asImageBitmap
@@ -13,6 +14,12 @@ actual fun ImageBitmap.toByteArray(): ByteArray? {
     this.asAndroidBitmap().compress(JPEG, 100, byteArrayOutputStream)
     val bytesArray = byteArrayOutputStream.toByteArray()
     return bytesArray
+}
+
+actual fun ImageBitmap.toPngByteArray(): ByteArray? {
+    val byteArrayOutputStream = ByteArrayOutputStream()
+    this.asAndroidBitmap().compress(PNG, 100, byteArrayOutputStream)
+    return byteArrayOutputStream.toByteArray()
 }
 
 actual fun Image.toImageBitmap(): ImageBitmap =
